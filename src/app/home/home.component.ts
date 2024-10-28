4dd13493f9a17dedc42a2f1d9fae7560d6dd4295
David Berry <dberry3755@gmail.com>
2024-10-28 12:17:44 -0400

    general layout working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productService: ProductService) {}
+  products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
+
   ngOnInit(): void {
     this.productService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
-      .subscribe((products) => {
-        console.log(products.items);
+      .subscribe((products: Products) => {
+        this.products = products.items;
       }),
       {};
   }
