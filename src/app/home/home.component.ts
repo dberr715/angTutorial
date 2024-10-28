73912ae2174df77a76315f58a2afa2b2c0f7bb82
David Berry <dberry3755@gmail.com>
2024-10-28 10:36:40 -0400

    loading product information into console for now

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  constructor(private productService: ProductService) {}
+  ngOnInit(): void {
+    this.productService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products) => {
+        console.log(products.items);
+      }),
+      {};
+  }
 }
