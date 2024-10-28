1d49ad7b07da6ef65f724c0ba2cdfcc18f19e48d
David Berry <dberry3755@gmail.com>
2024-10-28 13:25:00 -0400

    pagination working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductService } from '../services/product.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -19,12 +20,23 @@ export class HomeComponent {
     console.log(product, 'Output');
   }
 
-  ngOnInit(): void {
+  totalRecords: number = 0;
+  rows: number = 5;
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
     this.productService
-      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
       .subscribe((products: Products) => {
         this.products = products.items;
-      }),
-      {};
+        this.totalRecords = products.total;
+      });
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
   }
 }
