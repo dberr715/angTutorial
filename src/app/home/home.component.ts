8d47ae3d5269fad5cc604c1bb7234b4817154d97
David Berry <dberry3755@gmail.com>
2024-11-05 09:58:10 -0500

    everything working except prime icons imports

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
-import { PaginatorModule } from 'primeng/paginator';
+import { Paginator, PaginatorModule } from 'primeng/paginator';
 import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
 import { ButtonModule } from 'primeng/button';
 
@@ -21,15 +21,14 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  constructor(private productService: ProductService) {}
+  constructor(private productsService: ProductService) {}
+
+  @ViewChild('paginator') paginator: Paginator | undefined;
+
   products: Product[] = [];
 
-  onProductOutput(product: Product) {
-    console.log(product, 'Output');
-  }
-
   totalRecords: number = 0;
-  rows: number = 5;
+  rows: number = 12;
 
   displayEditPopup: boolean = false;
   displayAddPopup: boolean = false;
@@ -39,7 +38,14 @@ export class HomeComponent {
     this.displayEditPopup = true;
   }
 
-  toggleDeletePopup(product: Product) {}
+  toggleDeletePopup(product: Product) {
+    if (!product.id) {
+      return;
+    }
+
+    this.deleteProduct(product.id);
+  }
+
   toggleAddPopup() {
     this.displayAddPopup = true;
   }
@@ -53,34 +59,53 @@ export class HomeComponent {
   };
 
   onConfirmEdit(product: Product) {
-    this.editProduct(product, this.selectedProduct.id ?? 0);
+    if (!this.selectedProduct.id) {
+      return;
+    }
+
+    this.editProduct(product, this.selectedProduct.id);
     this.displayEditPopup = false;
   }
+
   onConfirmAdd(product: Product) {
     this.addProduct(product);
     this.displayAddPopup = false;
+  }
+
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
   }
 
   onPageChange(event: any) {
     this.fetchProducts(event.page, event.rows);
   }
 
+  resetPaginator() {
+    this.paginator?.changePage(0);
+  }
+
   fetchProducts(page: number, perPage: number) {
-    this.productService
+    this.productsService
       .getProducts('http://localhost:3000/clothes', { page, perPage })
-      .subscribe((products: Products) => {
-        this.products = products.items;
-        this.totalRecords = products.total;
+      .subscribe({
+        next: (data: Products) => {
+          this.products = data.items;
+          this.totalRecords = data.total;
+        },
+        error: (error) => {
+          console.log(error);
+        },
       });
   }
 
   editProduct(product: Product, id: number) {
-    this.productService
+    this.productsService
       .editProduct(`http://localhost:3000/clothes/${id}`, product)
       .subscribe({
         next: (data) => {
           console.log(data);
           this.fetchProducts(0, this.rows);
+          this.resetPaginator();
         },
         error: (error) => {
           console.log(error);
@@ -89,12 +114,13 @@ export class HomeComponent {
   }
 
   deleteProduct(id: number) {
-    this.productService
+    this.productsService
       .deleteProduct(`http://localhost:3000/clothes/${id}`)
       .subscribe({
         next: (data) => {
           console.log(data);
           this.fetchProducts(0, this.rows);
+          this.resetPaginator();
         },
         error: (error) => {
           console.log(error);
@@ -103,12 +129,13 @@ export class HomeComponent {
   }
 
   addProduct(product: Product) {
-    this.productService
+    this.productsService
       .addProduct(`http://localhost:3000/clothes`, product)
       .subscribe({
         next: (data) => {
           console.log(data);
           this.fetchProducts(0, this.rows);
+          this.resetPaginator();
         },
         error: (error) => {
           console.log(error);
